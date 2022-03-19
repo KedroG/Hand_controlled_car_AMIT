afbb0def48df8bbb313113a01b2c9d0c398b9fdb
KedroG <90105507+KedroG@users.noreply.github.com>
2022-03-19 20:39:45 +0200

    Add files via upload

---
+++ HandGesture_Car_Transimitter/ADC.c
@@ -0,0 +1,29 @@
+
+#include "ADC.h"
+
+void ADC_INIT(void)
+{
+	/*Voltage Reference*/
+	ADMUX = (ADC_SOURCE << 6) | (ADMUX & 0x3F);
+	/*Right ADJUST*/
+	Clear_Bit(ADMUX, 5);
+	/*128 Prescaler*/
+	ADCSRA = 0x07;	
+	/*Conversion Mode: AutoTrigger/FreeRunning*/
+	Clear_Bit(ADCSRA, 5);
+	SFIOR = (AUTO_TRIGGER_MODE << 5) | (SFIOR & 0x1F);
+	/*ADC peripheral Enable*/
+	Set_Bit(ADCSRA, 7);
+}
+Uint16 ADC_Read(Uint8 ADC_pin)
+{
+	/*Channel select*/
+	ADMUX = ADC_pin | (ADMUX & 0xE0);
+	/*Start of conversion*/
+	Set_Bit(ADCSRA, 6);
+	/*End of conversion Polling*/
+	while(Get_Bit(ADCSRA, 4) == 0);
+	/*Converted digital output*/
+	Uint16 DigitalOutput = ADCRA;
+	return DigitalOutput;
+}
---
+++ HandGesture_Car_Transimitter/ADC_CFG.h
@@ -0,0 +1,15 @@
+
+
+#ifndef ADC_CFG_H_
+#define ADC_CFG_H_
+
+#include "Bit_Math.h"
+#include "std.h"
+#include "MC_Selector.h"
+
+#define ADC_SOURCE                  AVCC
+#define AUTO_TRIGGER_MODE           FREE_RUNNING
+
+
+
+#endif /* ADC_CFG_H_ */
