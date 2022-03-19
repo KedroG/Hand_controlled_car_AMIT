eb926efdcd60db7e1a1eb0d81bcf9ba8fd8e8dd4
KedroG <90105507+KedroG@users.noreply.github.com>
2022-03-19 20:36:37 +0200

    Update main.c

--- HandGesture_Car_Transimitter/main.c
+++ HandGesture_Car_Transimitter/main.c
@@ -1,53 +1,68 @@
-#include "LIB/MCAL/ADC/ADC_Interface.h"
-#include "LIB/STD_TYPES.h"
-#include "LIB/MCAL/DIO/DIO_Interface.h"
-#include "LIB/MCAL/UART_DRIVER/UART_INTERFACE.h"
-#include "LIB/HAL/FlexSensor/FLEX_Interface.h"
+
+#include <util/delay.h>
+
+#include "ADC.h"
+#include "DIO.h"
+#include "UART_DRIVER/UART_INTERFACE.h"
 
 
-void main(){
 
-	HFLEX_voidInit();
-	ADC_voidInit();
+int main()
+{
+ 	float finger1 = 0;
+ 	float finger2 = 0;
+ 	float finger3 = 0;
+ 	float finger4 = 0;
+ 	float finger5 = 0;
+
+
+	ADC_INIT();
+	DIO_SetPinDir(DIO_PORTC,DIO_PIN_2,DIO_PIN_OUT);
 	UART_INIT();
+
 	while(1)
 	{
+		finger1 = (ADC_Read(ADC_PIN_0));
+		finger2 = (ADC_Read(ADC_PIN_1));
+		finger3 = (ADC_Read(ADC_PIN_2));
+		finger4 = (ADC_Read(ADC_PIN_3));
+		finger5 = (ADC_Read(ADC_PIN_4));
 
-		u8 u8_Finger1=HFLEX_u8Getangle(ADC_u8_CHANNEL_0);
-		u8 u8_Finger2=HFLEX_u8Getangle(ADC_u8_CHANNEL_1);
-		u8 u8_Finger3=HFLEX_u8Getangle(ADC_u8_CHANNEL_2);
-		u8 u8_Finger4=HFLEX_u8Getangle(ADC_u8_CHANNEL_3);
-		u8 u8_Finger5=HFLEX_u8Getangle(ADC_u8_CHANNEL_4);
+		if (finger1<=500&&finger2<=500&&finger3<=500&&finger4>=500) {
+					DIO_SetPinVal(DIO_PORTC,DIO_PIN_2,DIO_PIN_ON);
+					UART_Send('f');
+				}
 
-		if(u8_Finger1>=90&&u8_Finger2>=90&&u8_Finger3>=90&&u8_Finger4<=5)
-		{
-			UART_Send('f');
+		else if (finger1<=500&&finger2<=500&&finger3<=500&&finger4<=500) {
+					DIO_SetPinVal(DIO_PORTC,DIO_PIN_2,DIO_PIN_ON);
+					UART_Send('b');
+				}
 
+
+		else if (finger1>=500&&finger2<=500&&finger3<=500&&finger4<=500) {
+					DIO_SetPinVal(DIO_PORTC,DIO_PIN_2,DIO_PIN_ON);
+					UART_Send('r');
 		}
 
-		else if(u8_Finger1<=5&&u8_Finger2>=90&&u8_Finger3>=90&&u8_Finger4>=90)
-		{
-			UART_Send('r');
-
+		else if (finger1<=500&&finger2<=500&&finger3>=500&&finger4>=500) {
+					DIO_SetPinVal(DIO_PORTC,DIO_PIN_2,DIO_PIN_ON);
+					UART_Send('l');
 		}
 
-		else if(u8_Finger1>=90&&u8_Finger2>=90&&u8_Finger3<=5&&u8_Finger4<=5)
-		{
-			UART_Send('l');
-
+		else if (finger1<=500&&finger2<=500&&finger3<=500&&finger4<=500) {
+					DIO_SetPinVal(DIO_PORTC,DIO_PIN_2,DIO_PIN_ON);
+					UART_Send('s');
 		}
 
-		else if(u8_Finger1>=90&&u8_Finger2>=90&&u8_Finger3>=90&&u8_Finger4>=90)
-		{
-			UART_Send('b');
+		else {
+					DIO_SetPinVal(DIO_PORTC,DIO_PIN_2,DIO_PIN_OFF);
+					UART_Send('s');
+			}
 
-		}
-		else if(u8_Finger1<=15&&u8_Finger2<=15&&u8_Finger3<=15&&u8_Finger4<=15)
-		{
-			UART_Send('s');
-
-		}
-
+		_delay_ms(200);
 
 	}
+
 }
+
+
